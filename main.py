45eff69f33cfb137ffa72a7b4ab810cd14ba6d3e
Yuval Alfassi <yuvalalfassi@gmail.com>
2021-08-07 21:28:34 +0300

    Visualization works

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from utils.dataframes_utils import read_dataset, filter_dataset
-from utils.visualization_utils import visualize_histogram
-
-
+from utils.visualization_utils import visualize_histogram, visualize_scatter_plot
 
 
 def main():
     sale_price = 'SalePrice'
+    log_sale_price = 'Log(SalePrice)'
     columns_to_keep = ['LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'BsmtUnfSF', 'TotalBsmtSF',
                        'GrLivArea', 'TotRmsAbvGrd', 'GarageCars', 'GarageArea', 'YrSold', 'SalePrice']
     houses_prices_df = read_dataset('datasets/houses_prices.csv')
     houses_prices_df = filter_dataset(houses_prices_df, columns_to_keep)
-    visualize_histogram(houses_prices_df[sale_price].values, 'Price', 'Houses Prices')
-    visualize_histogram(np.log(houses_prices_df[sale_price].values), 'Log[Price]', 'Houses Prices [Log]')
+    houses_prices_df[log_sale_price] = np.log(houses_prices_df[sale_price])
+
+    for y_column in [sale_price, log_sale_price]:
+        visualize_histogram(houses_prices_df[y_column].values, y_column, 'Houses Prices')
+        for column_to_keep in columns_to_keep:
+            visualize_scatter_plot(houses_prices_df, column_to_keep, y_column)
+        # for build_options in (1,);
+        #     nn = build_full_connected_nn(*build_options)
 
 
 if __name__ == '__main__':
--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -1,12 +1,31 @@
+from functools import wraps
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualize_histogram(data, x_label, title):
-    plt.figure()
+def visualize_plot(func):
+    @wraps(func)
+    def returned_function(*args, **kwargs):
+        plt.figure()
+        ret_value = func(*args, **kwargs)
+        plt.tight_layout()
+        plt.show()
+        return ret_value
+    return returned_function
+
+
+@visualize_plot
+def visualize_histogram(data, x_label="", title=""):
     sns.kdeplot(data, color='g')
     plt.title(title)
     plt.xlabel(x_label)
-    plt.show()
+
+
+@visualize_plot
+def visualize_scatter_plot(dataframe, x_entry, y_entry):
+    sns.scatterplot(data=dataframe, x=x_entry, y=y_entry, alpha=0.5, color='g')
+    plt.xlabel(x_entry)
+    plt.ylabel(y_entry)
