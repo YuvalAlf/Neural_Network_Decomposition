47dd4e8a9d6a67ca09f7564cc4fc1f113603b7c6
Yuval Alfassi <yuvalalfassi@gmail.com>
2021-08-11 20:58:52 +0300

    Plots saved to filesystem

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from utils.visualization_utils import visualize_histogram, visualize_scatter_plo
 def main():
     sale_price = 'SalePrice'
     log_sale_price = 'Log(SalePrice)'
-    columns_to_keep = ['LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'BsmtUnfSF', 'TotalBsmtSF',
-                       'GrLivArea', 'TotRmsAbvGrd', 'GarageCars', 'GarageArea', 'YrSold', 'SalePrice']
+    sale_price_column = 'SalePrice'
+    x_columns = ['LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'BsmtUnfSF', 'TotalBsmtSF',
+                       'GrLivArea', 'TotRmsAbvGrd', 'GarageCars', 'GarageArea', 'YrSold']
     houses_prices_df = read_dataset('datasets/houses_prices.csv')
-    houses_prices_df = filter_dataset(houses_prices_df, columns_to_keep)
+    houses_prices_df = filter_dataset(houses_prices_df, x_columns + [sale_price_column])
     houses_prices_df[log_sale_price] = np.log(houses_prices_df[sale_price])
 
     for y_column in [sale_price, log_sale_price]:
-        visualize_correlation_matrix(houses_prices_df)
+        visualize_correlation_matrix(houses_prices_df, y_column)
         visualize_histogram(houses_prices_df[y_column].values, y_column, 'Houses Prices')
-        for column_to_keep in columns_to_keep:
-            visualize_scatter_plot(houses_prices_df, column_to_keep, y_column)
+        for x_column in x_columns:
+            visualize_scatter_plot(houses_prices_df, x_column, y_column)
         # for build_options in (1,);
         #     nn = build_full_connected_nn(*build_options)
 
--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -10,10 +10,11 @@ def visualize_plot(func):
     @wraps(func)
     def returned_function(*args, **kwargs):
         if VISUALIZE:
-            plt.figure()
+            plt.figure(figsize=[12, 8])
             ret_value = func(*args, **kwargs)
             plt.tight_layout()
-            plt.show()
+            # plt.show()
+            plt.close()
             return ret_value
     return returned_function
 
@@ -23,6 +24,7 @@ def visualize_histogram(data, x_label="", title=""):
     sns.kdeplot(data, color=COLOR)
     plt.title(title)
     plt.xlabel(x_label)
+    plt.savefig(f'plots/{x_label}_{title}.pdf', bbox_inches='tight')
 
 
 @visualize_plot
@@ -30,8 +32,10 @@ def visualize_scatter_plot(dataframe, x_entry, y_entry):
     sns.scatterplot(data=dataframe, x=x_entry, y=y_entry, alpha=0.5, color=COLOR)
     plt.xlabel(x_entry)
     plt.ylabel(y_entry)
+    plt.savefig(f'plots/{y_entry}_{x_entry}.pdf', bbox_inches='tight')
 
 
 @visualize_plot
-def visualize_correlation_matrix(dataframe):
-    sns.heatmap(dataframe.corr(), cmap=CMAP, linewidth=1, vmin=-1, vmax=1, annot=True, fmt='.1f')
+def visualize_correlation_matrix(dataframe, name):
+    sns.heatmap(dataframe.corr(), cmap=CMAP, linewidth=1, vmin=-1, vmax=1, annot=True, fmt='.2f')
+    plt.savefig(f'plots/{name}_Correlation_Matrix.pdf', bbox_inches='tight')
